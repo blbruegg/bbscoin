7b495d5e5b70015ae09d0bf080fb59d7f1a907d3
Ran Ding <randing89@gmail.com>
2018-02-25 03:17:30 -0800

    Add timestamp in tx results

--- src/Rpc/RpcServer.cpp
+++ src/Rpc/RpcServer.cpp
@@ -1034,6 +1034,7 @@ bool RpcServer::on_get_txs_by_height(const COMMAND_RPC_TXS_BY_HEIGHT::request& r
     txRecord.hash = txDetails.hash;
     txRecord.publicKey = txDetails.extra.publicKey;
     txRecord.fee = txDetails.fee;
+    txRecord.timestamp = txDetails.timestamp;
 
     // Create inputs
     for (const TransactionInputDetails txInputDetails : txDetails.inputs) {
