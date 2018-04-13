042de784ab50688e2a9f4138d4327efdd438cbea
Ran Ding <randing89@gmail.com>
2018-04-13 00:43:11 -0700

    Add cryptonight v7

--- src/CryptoNoteCore/CachedBlock.cpp
+++ src/CryptoNoteCore/CachedBlock.cpp
@@ -63,10 +63,14 @@ const Crypto::Hash& CachedBlock::getBlockLongHash(cn_context& cryptoContext) con
       const auto& rawHashingBlock = getBlockHashingBinaryArray();
       blockLongHash = Hash();
       cn_slow_hash(cryptoContext, rawHashingBlock.data(), rawHashingBlock.size(), blockLongHash.get());
-    } else if (block.majorVersion >= BLOCK_MAJOR_VERSION_2) {
+    } else if (block.majorVersion == BLOCK_MAJOR_VERSION_2 || block.majorVersion == BLOCK_MAJOR_VERSION_3) {
       const auto& rawHashingBlock = getParentBlockHashingBinaryArray(true);
       blockLongHash = Hash();
-      cn_slow_hash(cryptoContext, rawHashingBlock.data(), rawHashingBlock.size(), blockLongHash.get());
+      if (block.majorVersion == BLOCK_MAJOR_VERSION_2) {
+        cn_slow_hash(cryptoContext, rawHashingBlock.data(), rawHashingBlock.size(), blockLongHash.get());
+      } else {
+        cn_slow_hash_v7(cryptoContext, rawHashingBlock.data(), rawHashingBlock.size(), blockLongHash.get());
+      }
     } else {
       throw std::runtime_error("Unknown block major version.");
     }
